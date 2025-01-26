9ccd9c74935e81fc21505f1465ec8fc287983514
Asir Alam <alvi.abrar29@gmail.com>
2025-01-25 21:52:47 -0500

    Render markdown article files

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,18 +1,26 @@
-import ResearchItem from '@/components/ResearchItem'
+import { getSortedArticlesData } from '../../../lib/articles'
 
 export const metadata = {
   title: 'Off the Battlefield Foundation - Research',
   description: 'Helping veterans connect with compassionate companions.',
 }
 
-export default function Home() {
+export default function ResearchPage() {
+  const allArticlesData = getSortedArticlesData()
+
   return (
     <main className="pt-8 container mx-auto flex flex-col items-center">
-      {new Array(3).fill(0).map((_, i) => (
-        <div className="mb-32" key={i}>
-          <ResearchItem />
-        </div>
-      ))}
+      <ul>
+        {allArticlesData.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
     </main>
   )
 }
