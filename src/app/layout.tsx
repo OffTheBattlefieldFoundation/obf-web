ffdbd248b6bff09fa2f6deb521ccc67c963059a3
Asir Alam <alvi.abrar29@gmail.com>
2025-01-27 23:13:48 -0500

    Move SessionProvider to admin route group

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,19 @@ import './globals.css'
 import { ThemeProvider } from '@mui/material/styles'
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter'
 import theme from '@/app/theme'
-import { getServerSession } from 'next-auth'
-import SessionProvider from '@/components/SessionProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const session = await getServerSession()
-
   return (
     <html lang="en">
       <body className={inter.className + ' bg-lime-25 mt-24'}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <SessionProvider session={session}>
-            <ThemeProvider theme={theme}>{children}</ThemeProvider>
-          </SessionProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
