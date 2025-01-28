4ec181dc9d946c76311532b1fed9155d0a7e1991
Asir Alam <alvi.abrar29@gmail.com>
2025-01-28 16:57:22 -0500

    Add navbar dropdown button for publish page

--- src/app/research/page.tsx
+++ src/app/research/page.tsx
@@ -1,10 +1,6 @@
-import Link from 'next/link'
-
 import ResearchItem from '@/components/ResearchItem'
-import { Button } from '@/components/ui/button'
 
 import { getSortedArticlesMetaData } from '../../../lib/articles'
-
 export const metadata = {
   title: 'Off the Battlefield Foundation - Research',
   description: 'Helping veterans connect with compassionate companions.',
@@ -15,10 +11,6 @@ export default function ResearchPage() {
 
   return (
     <main className="pt-8 container mx-auto flex flex-col items-center">
-      <Button>
-        <Link href="/research/editor">Editor</Link>
-      </Button>
-
       {allArticlesMetaData.map((articleMetaData, i) => (
         <div key={i} className="mb-24">
           <ResearchItem articleMetaData={articleMetaData} />
--- src/app/research/publish/page.tsx
+++ src/app/research/publish/page.tsx
@@ -1,8 +1,8 @@
 import ArticleEditor from '@/components/ArticleEditor'
 
 export const metadata = {
-  title: 'Off the Battlefield Foundation - Article Editor',
-  description: 'Edit articles for the Off the Battlefield Foundation',
+  title: 'Off the Battlefield Foundation - Publish',
+  description: 'Publish articles for the Off the Battlefield Foundation',
 }
 
 export default function page() {
