676ab4d45225ce6c27da182b10e9b91c0a890bf7
AsirAAlam <alvi.abrar29@gmail.com>
2025-03-30 18:10:28 -0400

    Remove MUI 2/n

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react'
 import AppBar from '@mui/material/AppBar'
-import Toolbar from '@mui/material/Toolbar'
 import LogoTitle from '@/components/LogoTitle'
 import useScrollTrigger from '@mui/material/useScrollTrigger'
 import NavBarButton from '@/components/NavbarButton'
@@ -17,8 +16,8 @@ export default function MyNavbar() {
       className={trigger ? 'bg-lime-50' : 'bg-lime-25'}
       elevation={trigger ? 1 : 0}
     >
-      <div className={`${trigger ? 'py-2' : 'py-8'} px-16 transition-all`}>
-        <Toolbar>
+      <div className={`${trigger ? 'py-4' : 'py-10'} px-16 transition-all`}>
+        <div className="flex">
           <LogoTitle className="text-[#009b25]" />
 
           <div className="flex space-x-8 ml-12">
@@ -32,7 +31,7 @@ export default function MyNavbar() {
             </NavBarButton>
             <NavBarButton label="Donate" href="/donate" />
           </div>
-        </Toolbar>
+        </div>
       </div>
     </AppBar>
   )
--- src/components/NavbarButton.tsx
+++ src/components/NavbarButton.tsx
@@ -2,6 +2,7 @@
 import './NavbarButton.css'
 import { useState } from 'react'
 import Link from 'next/link'
+import useScrollTrigger from '@mui/material/useScrollTrigger'
 
 type NavBarButtonProps = {
   label: string
@@ -16,6 +17,10 @@ export default function NavbarButton({
   children,
 }: NavBarButtonProps) {
   const [isHovering, setIsHovering] = useState<boolean>(false)
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 0,
+  })
 
   // If there is a single child, use an array of size 1
   const childrenArray: React.ReactNode[] | null =
@@ -26,7 +31,10 @@ export default function NavbarButton({
       : null
 
   return (
-    <div onMouseLeave={() => setIsHovering(false)}>
+    <div
+      onMouseLeave={() => setIsHovering(false)}
+      className="flex flex-col justify-center relative"
+    >
       <Link href={href}>
         <button
           className="mybutton bg-gradient-to-r from-lime-600 from-50% to-lime-950 to-50%"
@@ -37,15 +45,17 @@ export default function NavbarButton({
       </Link>
 
       {isHovering && childrenArray != null && (
-        <div style={{ position: 'fixed' }}>
-          <div className="h-2" />
-          <div className="min-w-20 p-2 flex flex-col bg-stone-50 rounded-md shadow-xl">
-            {childrenArray.map((child, i) => (
-              <div key={i} className="py-2">
-                {child}
-              </div>
-            ))}
-          </div>
+        <div
+          className={
+            'fixed min-w-20 p-2 flex flex-col bg-stone-50 rounded-md shadow-xl transition-all'
+          }
+          style={{ top: trigger ? 60 : 84 }}
+        >
+          {childrenArray.map((child, i) => (
+            <div key={i} className="py-2">
+              {child}
+            </div>
+          ))}
         </div>
       )}
     </div>
